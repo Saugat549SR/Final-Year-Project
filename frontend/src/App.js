d7ef711beb26dadda35501cfa251140a1f26cfbf
Saugat Karki <np03cs4s210146@heraldcollege.edu.np>
2022-11-23 17:56:09 +0545

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header >
+        Bike Barn
+      </header>
+    </div>
+  );
+}
+
+export default App;
