255edb5a6a77aff9c283c6b056fbeb1b5bdac31b
Saugat Karki <np03cs4s210146@heraldcollege.edu.np>
2023-04-09 10:34:43 +0545

    Date picker in rent

--- frontend/src/components/Main/Rents/RentDetails.jsx
+++ frontend/src/components/Main/Rents/RentDetails.jsx
@@ -1,23 +1,37 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import Carousel from 'react-material-ui-carousel';
 import './RentDetails.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { getRentDetails } from '../../../actions/rentAction';
 import { useParams } from 'react-router-dom';
-
 import { Navbar } from '../Homepage/Navbar';
 import { useAlert } from 'react-alert';
-
+import { DatePicker } from 'antd';
+import moment from 'moment';
+import { DateTime } from 'luxon';
+const { RangePicker } = DatePicker;
 export const RentDetails = () => {
   const dispatch = useDispatch();
   const alert = useAlert();
   const { rent, loading, error } = useSelector((state) => state.rentDetails);
   const params = useParams();
+  const [from, setFrom] = useState();
+  const [to, setTo] = useState();
+  const [totalDays, setTotalDays] = useState(0);
 
   useEffect(() => {
     dispatch(getRentDetails(params.id));
   }, [dispatch, params.id]);
 
+  function selectTimeSlots(values) {
+    setFrom(
+      DateTime.fromJSDate(values[0].toDate()).toFormat('MMM dd yyyy HH:mm')
+    );
+    setTo(
+      DateTime.fromJSDate(values[1].toDate()).toFormat('MMM dd yyyy HH:mm')
+    );
+    setTotalDays(values[1].diff(values[0], 'Days'));
+  }
   return (
     <Fragment>
       <Navbar />
@@ -39,7 +53,7 @@ export const RentDetails = () => {
 
           <div className="right">
             <h1 className="product-title">{rent.name}</h1>
-            <h2 className="product-price">{`Rs ${rent.price}`} per/hour</h2>
+            <h2 className="product-price">{`Rs ${rent.price}`} per/day</h2>
             <p className="product-description">{rent.description}</p>
             <p>
               Status:
@@ -47,6 +61,17 @@ export const RentDetails = () => {
                 {rent.stock < 1 ? 'OutOfStock' : 'InStock'}
               </b>
             </p>
+            <RangePicker
+              showTime={{ format: 'HH:mm' }}
+              format="MMM DD YYYY HH:mm"
+              onChange={selectTimeSlots}
+            />
+            <div>
+              <p>Total Days: {totalDays}</p>
+            </div>
+            <div>
+              <h2>Total Amount: {rent.price * totalDays}</h2>
+            </div>
 
             <div className="cart-buttons">
               <button className="add-to-cart-button">CheckOut</button>
